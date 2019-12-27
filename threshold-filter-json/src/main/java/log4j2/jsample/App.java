4cf87621b7f108895011ff1c6a255e64a776a77b
SACHDEVA Vikas <vikas.sachdeva@soprabanking.com>
2019-12-27 22:16:11 +0530

    threshold filter in json configuration

---
+++ threshold-filter-json/src/main/java/log4j2/jsample/App.java
@@ -0,0 +1,24 @@
+package log4j2.jsample;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Name of the log configuration file must be log4j2.json and It should be
+ * present in the classpath of the program.
+ */
+
+public class App {
+	private static final Logger LOGGER = LogManager.getLogger(App.class);
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			if (i % 2 == 0)
+				LOGGER.info("It is info log -  {}", i);
+			else
+				LOGGER.warn("It is warn log - {} ", i);
+		}
+		LOGGER.error("It is error log");
+	}
+}
